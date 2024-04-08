e0b33395e63a07d9e7d25f3faa632e29eb0e18cc
Cayo Cesar <96149641+Cayo-Cesar@users.noreply.github.com>
2024-04-08 07:57:21 -0300

    Adiciona tratamento de exceção e mensagens de erro para operações de busca, listagem e exclusão de usuários

--- src/biblioteca/gerenciadorUsuarios.java
+++ src/biblioteca/gerenciadorUsuarios.java
@@ -11,21 +11,44 @@ public class gerenciadorUsuarios {
     }
 
     public void adicionarUsuario(usuario usuario) {
+        try {
+            usuario.set_id_usuario(usuarios.size() + 1);
+        } catch (Exception e) {
+            usuario.set_id_usuario(1);
+        }
         usuarios.put(usuario.get_id_usuario(), usuario);
     }
 
     public usuario buscarUsuario(int idUsuario) {
-        return usuarios.get(idUsuario);
+        try {
+            return usuarios.get(idUsuario);
+        } catch (Exception e) {
+            System.out.println("Usuário não encontrado.");
+        }
+        return null;
     }
 
     public void listarUsuarios() {
-        System.out.println("Lista de Usuários:");
-        for (usuario usuario : usuarios.values()) {
-            System.out.println("ID: " + usuario.get_id_usuario() + ", Nome: " + usuario.get_nome());
+        try {
+            System.out.println("Lista de Usuários:");
+            for (usuario usuario : usuarios.values()) {
+                System.out.println("ID: " + usuario.get_id_usuario() + ", Nome: " + usuario.get_nome());
+            }
+        } catch (Exception e) {
+            System.out.println("Não há usuários cadastrados.");
         }
     }
 
     public void excluirUsuario(int idUsuario) {
-        usuarios.remove(idUsuario);
+        try {
+            for (usuario usuario : usuarios.values()) {
+                if (usuario.get_id_usuario() > idUsuario) {
+                    usuario.set_id_usuario(usuario.get_id_usuario() - 1);
+                    usuarios.remove(idUsuario);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Usuário não encontrado.");
+        }
     }
 }
