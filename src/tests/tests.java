39703d9c9091b9a3b5afa63eb6f7762430aa0e75
Cayo Cesar <96149641+Cayo-Cesar@users.noreply.github.com>
2024-04-15 18:07:40 -0300

    Atualizações de código

---
+++ src/tests/tests.java
@@ -0,0 +1,15 @@
+package tests;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class tests {
+    @Test
+    public void test1() {
+        assertEquals(1, 1);
+    }
+}
+
+
+
+
